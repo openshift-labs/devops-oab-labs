e194e494a23c7a62c317f619264b259eeceee934
Siamak Sadeghianfar <siamaksade@gmail.com>
2018-02-28 18:19:22 +0100

    enabled CORS on catalog

--- catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/CatalogController.java
+++ catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/CatalogController.java
@@ -8,12 +8,14 @@ import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/api/products")
+@CrossOrigin
 public class CatalogController {
 
 	  @Autowired
