c94648ccc0cb230f28ec21d2905013594a70ba9d
Siamak Sadeghianfar <ssadeghi@redhat.com>
2018-05-01 17:39:47 +0200

    update catalog code

--- catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/CatalogController.java
+++ catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/CatalogController.java
@@ -1,10 +1,5 @@
 package com.redhat.cloudnative.catalog;
 
-import java.util.List;
-import java.util.Spliterator;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -12,6 +7,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping(value = "/api/products")
@@ -25,6 +25,7 @@ public class CatalogController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getAll() {
         Spliterator<Product> products = repository.findAll().spliterator();
-        return StreamSupport.stream(products, false).collect(Collectors.toList());
+        List<Product> list = StreamSupport.stream(products, false).collect(Collectors.toList());
+        return list;
     }
 }
